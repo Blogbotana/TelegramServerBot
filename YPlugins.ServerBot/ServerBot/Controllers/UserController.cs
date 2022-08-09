7213c1bc8dabe8c149395c8dae5db00809dbe961
Misha <bel692@mail.ru>
2022-08-09 19:10:33 +0300

    Added UserController

---
+++ YPlugins.ServerBot/ServerBot/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerBot.Services;
+
+namespace ServerBot.Controllers
+{
+
+    [ApiController]
+    [Route("/")]
+    public class UserController
+    {
+        private readonly ILogger<UserController> _logger;
+        private TgUserService tgUserService = new TgUserService();
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
