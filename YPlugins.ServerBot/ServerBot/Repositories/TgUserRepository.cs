e8918269947942681c5ea60c5112d86781162512
Misha <bel692@mail.ru>
2022-08-11 18:24:20 +0300

    Added more functions to controller

--- YPlugins.ServerBot/ServerBot/Repositories/TgUserRepository.cs
+++ YPlugins.ServerBot/ServerBot/Repositories/TgUserRepository.cs
@@ -22,13 +22,17 @@ namespace ServerBot.Repositories
                 if (lang == null)
                     lang = LanguageRepository.GetDefaultLanguage();
 
+                var user = applicationContext.Users.Where(u=>u.TgId == userEntity.TgId).FirstOrDefault();
+                if (user != null)
+                    return 0;
+
                 userEntity.Language = lang;
                 applicationContext.Users.Add(userEntity);
                 return applicationContext.SaveChanges();
             }
         }
 
-        public static UserEntity GetUserByTgId(long userTgId)
+        public static UserEntity? GetUserByTgId(long userTgId)
         {
             using (ApplicationContext context = new ApplicationContext())
             {
@@ -40,6 +44,43 @@ namespace ServerBot.Repositories
                 context.Entry(user).Reference(t => t.Language).Load();
                 context.Entry(user).Collection(c => c.Licenses).Load();
                 return user;
+            }
+        }
+
+        public static UserEntity? GetUserByEmail(string email)
+        {
+            using (ApplicationContext context = new ApplicationContext())
+            {
+                UserEntity user = context.Users.Where(u => u.Email.ToLower() == email.ToLower()).FirstOrDefault();
+
+                if (user == null)
+                    return null;
+
+                context.Entry(user).Reference(t => t.Language).Load();
+                context.Entry(user).Collection(c => c.Licenses).Load();
+                return user;
+            }
+        }
+
+        public static bool EditUser(UserEntity userEntity)
+        {
+            using (ApplicationContext context = new ApplicationContext())
+            {
+                var user = context.Users.Where(u => u.Id == userEntity.Id).FirstOrDefault();
+                if (user == null)
+                    return false;
+                //I don't know is it ok or no
+                user.Email = userEntity.Email;
+                user.ComputerInformation = userEntity.ComputerInformation;
+                user.Licenses = userEntity.Licenses;
+                user.Language = userEntity.Language;
+                user.LastLogin = userEntity.LastLogin;
+                user.FirstName = userEntity.FirstName;
+                user.LastName = userEntity.LastName;
+                user.TgId = userEntity.TgId;
+
+                context.SaveChanges();
+                return true;
             }
         }
     }
